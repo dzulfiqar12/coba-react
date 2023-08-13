8a001bcdec63b48a0768284d4f8cbffbf0a39cd3
dzulfiqar12 <rdzulfiqar64@gmail.com>
2023-08-13 21:45:38 +0700

    finalization

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -5,17 +5,20 @@ function VideoDetail() {
     const id = useParams().id;
     const [products, setProducts] = useState([]);
     const [comments, setComments] = useState([]);
-    useEffect(() => {
+    const [video, setVideo] = useState([]);
+    useEffect(() => { 
         axios.get(`https://erin-nice-zebra.cyclic.app/api/video/${id}/products`).then((response) => {
             // console.log(response.data[0].products);
             setProducts(response.data[0].products);
-            console.log(products);
+            // console.log(response.data[0].url_video);
+            setVideo(response.data[0].url_video);
+            // console.log(products);
         });
         axios.get(`https://erin-nice-zebra.cyclic.app/api/video/${id}/comments`).then((response) => {
             // console.log(response.data[0].products);
             // setProducts(response.data[0].products);
             setComments(response.data[0].comments);
-            console.log(response.data[0].comments);
+            // console.log(response.data[0].comments);
         });
 
     }, []);
@@ -24,10 +27,13 @@ function VideoDetail() {
 
         <div className="grid-container">
             <div className="left">
+            <h2>Products:</h2>
                 {
                     products.map((result, index) => {
                         return (
-                            <a href={result.link_product} className="product" key={index}> <p>{result.title}</p><p>{result.price}</p>
+                            <a href={result.link_product} className="product" key={index}> 
+                            <p>{result.title}</p>
+                            <p> {result.price}</p>
                             </a>);
                     })
                 }
@@ -35,7 +41,7 @@ function VideoDetail() {
             </div>
             <div className="center">
                 <iframe width="80%" height="315"
-                    src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1">
+                    src={video}>
                 </iframe>
             </div>
             <div className="right">
@@ -45,7 +51,7 @@ function VideoDetail() {
                         comments.map((result, index) => {
                             return (
                                 <div className="comment-wrapper" key={index}>
-                                    <h4>{result.username}</h4>
+                                    <h4>{result.username} : </h4>
                                     <p>{result.comment}</p>
                                 </div>
 
@@ -54,10 +60,10 @@ function VideoDetail() {
                     }
                 </div>
                 <div className="add-comment-section">
-                    <input id="username" type="text" placeholder="username" />
-                    <textarea id="comment" type="text" placeholder="comment" />
-                    <br />
-                    <button id="submitButton" type="" onClick={submitComment} data-id={id}>Submit</button>
+                    <h4>Add Comment: </h4>
+                        <input id="username" type="text" placeholder="username" />
+                        <textarea id="comment" type="text" placeholder="comment" />
+                        <button id="submitButton" type="" onClick={submitComment} data-id={id}>Submit</button>
                 </div>
             </div>
         </div>
